04ad359b712be365c67b6b587d0f8963ed69b7ff
Lola Martorell <lolamara29@gmail.com>
2024-11-26 19:13:20 +0100

    Final evaluación 2

--- EV.2.sql
+++ EV.2.sql
@@ -145,3 +145,9 @@ WHERE film.film_id IN (
   )
 );
 
+-- 17. Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla film
+
+SELECT title
+FROM film
+WHERE rating = 'R'
+AND length > 120;
