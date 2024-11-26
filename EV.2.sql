2b10c9de422050e1c2454a6ddabb9ff8a75df47e
Lola Martorell <lolamara29@gmail.com>
2024-11-26 18:38:38 +0100

    Evaluación_final_M2

--- EV.2.sql
+++ EV.2.sql
@@ -87,3 +87,61 @@ customer.customer_id, customer.first_name, customer.last_name;
 -- Ejercicio 11: Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el recuento de alquileres
 
 
+
+-- Ejercicio 12: Encuentra el promedio de duración de las películas para cada clasificación de la tabla film y muestra la clasificación junto con el promedio de duración
+
+SELECT 
+rating AS clasificación, 
+AVG(length) AS promedio_duración_peliculas
+FROM film 
+GROUP BY 
+clasificación;
+
+-- Ejercicio 13: Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love"
+
+SELECT 
+first_name AS nombre_actor,
+last_name AS apellido_actor
+FROM actor 
+INNER JOIN film_actor 
+ON actor.actor_id = film_actor.actor_id
+INNER JOIN film
+ON film_actor.film_id = film.film_id
+WHERE 
+UPPER(film.title) = 'Indian Love';
+
+# En este caso usamos UPPER para garantizar una comparación exacta y sin distinción de mayúsculas y minúsculas.
+# También podríamos usar LOWER para convertir todo a minúscula. 
+
+-- Ejercicio 14: Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción
+
+SELECT 
+title,
+description
+FROM film
+WHERE description LIKE '%dog%' 
+OR description LIKE '%cat%'; 
+
+-- Ejercicio 15: Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010
+
+SELECT
+title,
+release_year
+FROM film
+WHERE release_year BETWEEN 2005 AND 2010;
+
+-- Ejercicio 16:  Encuentra el título de todas las películas que son de la misma categoría que "Family"
+
+SELECT 
+film.title AS Título
+FROM film 
+WHERE film.film_id IN (
+  SELECT film_category.film_id
+  FROM film_category
+  WHERE film_category.category_id = (
+    SELECT category_id
+    FROM category
+    WHERE name = 'Family'
+  )
+);
+
